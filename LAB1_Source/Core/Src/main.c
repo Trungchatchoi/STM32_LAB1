c89c14cbb0a733a014be9d173d4f317e7520f7d5
Trungchatchoi <trung.pham01152224@hcmut.edu.vn>
2023-09-18 21:57:47 +0700

    Init EX8

--- LAB1_Source/Core/Src/main.c
+++ LAB1_Source/Core/Src/main.c
@@ -52,6 +52,45 @@ static void MX_GPIO_Init(void);
 void clearAllClock(){
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All, 1);
 }
+void setNumberOnClock(int num){
+	switch(num){
+	case 0:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 0);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 0);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 0);
+		break;
+	case 9:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, 0);
+		break;
+	case 10:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, 0);
+		break;
+	default:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 0);
+	}
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
